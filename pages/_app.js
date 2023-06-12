c9fc33a7bd553b43a1008ed65ef78fb97ed61c8c
Mrutunjay113 <mrutunjay765@gmail.com>
2023-06-12 22:04:56 +0530

    updated  index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,41 @@
-import '@/styles/globals.css'
+import Loading from "@/components/loading/loading";
+import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { magic } from "../lib/magic-client";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      setIsLoading(false);
+      if (isLoggedIn) {
+        //  Route to /
+        setIsLoading(false);
+        router.push("/");
+      } else {
+        // to /login
+        setIsLoading(false);
+        router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  //return <Component {...pageProps} />;
+  return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
